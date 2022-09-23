54503924d4c0f08740967ae5e5a51841735e99b5
PolopTech <73751987+PolopTechnology@users.noreply.github.com>
2022-09-22 20:53:36 -0400

    Add files via upload

---
+++ public/react.js
@@ -0,0 +1,24 @@
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked comment number ' + this.props.commentID;
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#female_that_gave_you_birth');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
